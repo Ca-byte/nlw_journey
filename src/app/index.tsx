aaff4e06ae79bda2a5b676ef6d32c35eed4e6ae7
Caroline Vieira <carolinesanvieira@gmail.com>
2024-07-10 19:15:55 -0300

    class two - wip

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,98 @@
 import { Button } from "@/components/button";
+import { Calendar } from "@/components/calendar";
+import { GuestEmail } from "@/components/email";
 import { Input } from "@/components/input";
+import { Modal } from "@/components/modal";
 import { colors } from "@/styles/colors";
-import { ArrowRight, Calendar as IconCalendar, MapPin, Settings2, UserRoundPlus } from "lucide-react-native";
+import { calendarUtils, DatesSelected } from "@/utils/calendarUtils";
+import { validateInput } from "@/utils/validateInput";
+import dayjs from "dayjs";
+import { ArrowRight, AtSign, Calendar as IconCalendar, MapPin, Settings2, UserRoundPlus } from "lucide-react-native";
 import { useState } from "react";
-import { Image, Text, View } from "react-native";
+import { Alert, Image, Keyboard, Text, View } from "react-native";
+import { DateData } from "react-native-calendars";
 
 enum StepForm {
 	TRIP_DETAILS = 1,
 	ADD_EMAIL = 2
 }
 
+enum MODAL {
+  NONE = 0,
+  CALENDAR = 1,
+  GUESTS = 2,
+}
+
 export default function Index(){
+	//DATA
 	const [stepForm, setStepForm]= useState(StepForm.TRIP_DETAILS)
+	const [selectedDates, setSelectedDates] = useState({} as DatesSelected)
+	const [destination, setDestination] = useState("")
+  const [emailToInvite, setEmailToInvite] = useState("")
+  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([])
+
+
+	 // MODAL
+	 const [showModal, setShowModal] = useState(MODAL.NONE)
 
 	function handleNextStepForm(){
+		if (
+      destination.trim().length === 0 ||
+      !selectedDates.startsAt ||
+      !selectedDates.endsAt
+    ) {
+      return Alert.alert(
+        "Trip Details",
+				"Fill in all the trip information to proceed."
+      )
+    }
+
+    if (destination.length < 4) {
+      return Alert.alert(
+        "Trip Details",
+				"The destination must be at least 4 characters long."			
+      )
+    }
+
 		if(stepForm === StepForm.TRIP_DETAILS){
 			return setStepForm(StepForm.ADD_EMAIL)
 		}
 	}
+
+
+	function handleSelectDate(selectedDay: DateData) {
+    const dates = calendarUtils.orderStartsAtAndEndsAt({
+      startsAt: selectedDates.startsAt,
+      endsAt: selectedDates.endsAt,
+      selectedDay,
+    })
+
+    setSelectedDates(dates)
+  }
+
+	function handleRemoveEmail(emailToRemove: string) {
+    setEmailsToInvite((prevState) =>
+      prevState.filter((email) => email !== emailToRemove)
+    )
+  }
+
+	function handleAddEmail() {
+    if (!validateInput.email(emailToInvite)) {
+      return Alert.alert("Convidado", "E-mail inválido!")
+    }
+
+    const emailAlreadyExists = emailsToInvite.find(
+      (email) => email === emailToInvite
+    )
+
+    if (emailAlreadyExists) {
+      return Alert.alert("Convidado", "E-mail já foi adicionado!")
+    }
+
+    setEmailsToInvite((prevState) => [...prevState, emailToInvite])
+    setEmailToInvite("")
+  }
+
 
 
 	return(
@@ -43,6 +118,8 @@ export default function Index(){
 					<Input.Field 
 						placeholder="Where we go?" 
 						editable={stepForm === StepForm.TRIP_DETAILS} 
+						onChangeText={setDestination}
+						value={destination}
 					/>
 				</Input>
 
@@ -51,6 +128,12 @@ export default function Index(){
 					<Input.Field 
 						placeholder="When?" 
 						editable={stepForm === StepForm.TRIP_DETAILS}
+						onFocus={() => Keyboard.dismiss()}
+            showSoftInputOnFocus={false}
+						onPressIn={() =>
+              stepForm === StepForm.TRIP_DETAILS && setShowModal(MODAL.CALENDAR)
+            }
+						value={selectedDates.formatDatesInText}
 						/>
 				</Input>
 				{stepForm === StepForm.ADD_EMAIL && 
@@ -64,7 +147,19 @@ export default function Index(){
 
 					<Input>
 						<UserRoundPlus color={colors.zinc[400]} size={20} />
-						<Input.Field placeholder="Who will be on the trip?" />
+						<Input.Field placeholder="Who will be on the trip?" 
+						autoCorrect={false}
+						value={
+							emailToInvite.length > 0 
+							 ? `${emailsToInvite.length} guest(s) invited`
+                    : ""
+						}
+						onPress={() => {
+							Keyboard.dismiss()
+							setShowModal(MODAL.GUESTS)
+						}}
+						showSoftInputOnFocus={false}
+						/>
 					</Input>
 					</>
 				}
@@ -85,6 +180,65 @@ export default function Index(){
 					terms of use and privacy policies.
 				</Text>
 			</Text>
+			<Modal
+				title="Select guests"
+				subtitle="Guests will receive emails to confirm their participation in the trip."
+				visible={showModal === MODAL.CALENDAR}
+        onClose={() => setShowModal(MODAL.NONE)}
+			>
+				 <View className="gap-4 mt-4">
+          <Calendar
+            minDate={dayjs().toISOString()}
+            onDayPress={handleSelectDate}
+            markedDates={selectedDates.dates}
+          />
+
+          <Button onPress={() => setShowModal(MODAL.NONE)}>
+            <Button.Title>Confirm</Button.Title>
+          </Button>
+        </View>
+			</Modal>
+			
+			<Modal
+        title="Select Guests"
+        subtitle="Guests will receive emails to confirm their participation in the trip."
+        visible={showModal === MODAL.GUESTS}
+        onClose={() => setShowModal(MODAL.NONE)}
+      >
+        <View className="my-2 flex-wrap gap-2 border-b border-zinc-800 py-5 items-start">
+          {emailsToInvite.length > 0 ? (
+            emailsToInvite.map((email) => (
+              <GuestEmail
+                key={email}
+                email={email}
+                onRemove={() => handleRemoveEmail(email)}
+              />
+            ))
+          ) : (
+            <Text className="text-zinc-600 text-base font-regular">
+             No email added.
+            </Text>
+          )}
+        </View>
+
+        <View className="gap-4 mt-4">
+          <Input variant="secondary">
+            <AtSign color={colors.zinc[400]} size={20} />
+            <Input.Field
+              placeholder="Enter guest's email"
+              keyboardType="email-address"
+              onChangeText={(text) => setEmailToInvite(text.toLowerCase())}
+              value={emailToInvite}
+              returnKeyType="send"
+              onSubmitEditing={handleAddEmail}
+            />
+          </Input>
+
+          <Button onPress={handleAddEmail}>
+            <Button.Title>Invite</Button.Title>
+          </Button>
+        </View>
+      </Modal>
 		</View>
 	)
 }
--- src/utils/dayjsLocaleConfig.ts
+++ src/utils/dayjsLocaleConfig.ts
@@ -1,5 +1,9 @@
-import "dayjs/locale/pt-br"
 import dayjs from "dayjs"
+import "dayjs/locale/en"
+import "dayjs/locale/pt-br"
 
 // Para traduzir os nomes de dias e mês para pt-br.
 dayjs.locale("pt-br")
+
+// To translate the names of days and months to English.
+dayjs.locale("en")
